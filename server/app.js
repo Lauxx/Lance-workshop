8f4cdb25ddceeaa4d6a26a91860dace5a48c8395
Lauxx <lm.nichols@hotmail.com>
2016-04-04 16:43:12 -0600

    adding XML responses to app.js

--- server/app.js
+++ server/app.js
@@ -6,6 +6,15 @@ app.get('/sms', function getCb(req, res) {
 
 
 	console.log(req.body);
+	var twiml = [
+    			'<?xml version="1.0" encoding="UTF-8" ?>',
+    			'<Response>',
+    			'  <Message>You said: ' + req.body.Body + '</Message>',
+    			'</Response>'
+  				].join('');
+
+  res.set('Content-Type', 'text/xml');
+  res.send(twiml);
 
 
 
