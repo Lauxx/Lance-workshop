4db57e0fccb3df25e39642dd88c1bb498d691b5c
Lauxx <lm.nichols@hotmail.com>
2016-04-04 14:45:28 -0600

    successfully sending a text message to myself

---
+++ sms.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+
+
+console.log('config', require('./config'));
+
+
+var config = require('./config');
+var twilio = require('twilio')(config.twilio.accountSid, config.twilio.authToken);
+
+twilio.messages.create({
+  body: 'Hello Lotus',
+  to: config.myPhone,
+  from: config.twilio.from
+}, function createCb(err, data) {
+  if (err) return console.log('error sending message!', err);
+  return console.log('sent message', data);
+});
+
