595b9b51f40f794655089a8519cb55dba580d4a9
Lauxx <lm.nichols@hotmail.com>
2016-04-04 16:36:30 -0600

    added body parser middleware

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
+/* eslint-disable no-console, vars-on-top  */
 var express = require('express');
+var config = require('./config');
 var app = express();
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/twilio');
-var port = process.env.PORT || 8000;
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function (req, res){
-	res.json({message: 'Hello world'})
+if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+} else {
+  // Do "hot-reloading" of express stuff on the server
+  // Throw away cached modules and re-require next time
+  // Ensure there's no important state in there!
+  var chokidar = require('chokidar');
+  var watcher = chokidar.watch('./server');
+  watcher.on('ready', function onReady() {
+    watcher.on('all', function onAll() {
+      console.log('Clearing /server/ module cache from server');
+      Object.keys(require.cache).forEach(function each(id) {
+        if (/\/server\//.test(id)) delete require.cache[id];
+      });
+    });
+  });
+}
+
+// Include server routes as a middleware
+app.use(function useCb(req, res, next) {
+  require('./server/app')(req, res, next);
 });
 
-app.listen(port);
-console.log('winning on ' + port);
+// Start the webserver
+var port = config.port;
+var hostname = config.hostname;
+app.listen(port, hostname, function listenCb(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Listening at http://' + hostname + ':' + port);
+});
+
+
---
+++ server/app.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+var express = require('express');
+var app = new express.Router();
+
+app.get('/sms', function getCb(req, res) {
+
+
+	console.log(req.body);
+
+
+
+  res.send({ ok: true });
+});
+
+module.exports = app;
